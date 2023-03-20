6f88fa5b4e3de547213d526cca5a133d8975bfef
KramesDt <lordkratos365@gmail.com>
2023-03-20 13:23:30 +0100

    Home page almost done

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import "./styles.css"
+
 
 const Home = () => {
   return (
     <>
       <div className='hero-img'>
-        <img alt='heroimg' src='https://images.unsplash.com/photo-1446776653964-20c1d3a81b06?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80' />
+        <img alt='heroimg' className='hero' src='https://images.unsplash.com/photo-1446776653964-20c1d3a81b06?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80' />
+        <div className='hero-text'>
+          <h1 className='hero-text-main'>SMARTX Entreprise</h1>
+          <h1 className='hero-text-sub'>Redefining Excellence Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation  </h1>
+        </div>
       </div>
     </>
   )
