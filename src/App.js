c41f8c99aa9b2b5e06853ab5dc2cfd3deadb413e
KramesDt <lordkratos365@gmail.com>
2023-03-21 12:46:24 +0100

    technology page bug

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import About from './components/about';
 import Header from './components/header';
 import Home from './components/home';
+import Technology from './components/technology';
 import "./styles.css"
 
 function App() {
   return (
     <>
-      <Header/>
+      <Header />
       <Home />
       <About />
+      <Technology />
     </>
   );
 }
--- src/components/about/index.js
+++ src/components/about/index.js
@@ -1,16 +1,21 @@
 import React from 'react'
 import './styles.css'
 
+const imgLink = 'https://images.unsplash.com/photo-1531545514256-b1400bc00f31?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80'
+
 const About = () => {
   return (
-    <section className='about-box'>
-        <div className='image-box'>
-            image
+    <>
+      <section className='about-box'>
+        <div className='float image-box'>
+            <img className='about-img' src={imgLink} />
         </div>
-        <div className='text-box'>
-            some text
+        <div className='float text-box'>
+            <h2 className='some-text'>About US</h2>
+            <p className=''> "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
         </div>
     </section>
+    </>
   )
 }
 
