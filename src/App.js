1d57225639d1ce0d6096b18ca20308ee16f1b270
KramesDt <lordkratos365@gmail.com>
2023-04-05 10:28:53 +0100

    Initialized footer component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import About from './components/about';
 import Header from './components/header';
 import Home from './components/home';
-import Technology from './components/technology';
+import Footer from './components/footer';
 import "./styles.css"
 
 function App() {
@@ -33,6 +33,7 @@ function App() {
           <p className=''> "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
         </div>
       </About>
+      <Footer/>
     </>
   );
 }
