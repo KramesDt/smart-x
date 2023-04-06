9c9f3ab9dc7811117abb8c215b901cbff8fa5f0b
KramesDt <lordkratos365@gmail.com>
2023-04-06 10:00:30 +0100

    initialised contact page and fixed a backgroung img

---
+++ src/components/contact/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './style.css'
+
+const Contact = () => {
+  return (
+    <div className='contact-box'>
+        contact
+    </div>
+  )
+}
+
+export default Contact
